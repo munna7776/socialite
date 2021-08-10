0da055a2f1afc4b28e01f25958f0756c908f9d89
Munna <barnwalmunnakumar@gmail.com>
2021-08-10 15:24:36 +0530

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row mt-2">
+                            <div class="col-md-6 offset-md-4">
+                                <a href="{{route('login.google')}}" class="btn btn-danger text-center w-100 fw-bold ">Login with Google</a>
+                                <a href="{{route('login.facebook')}}" class="btn text-center w-100 fw-bold mt-2" style="background-color: blue;">Login with Facebook</a>
+                                <a href="{{route('login.github')}}" class="btn btn-dark text-center w-100 fw-bold mt-2">Login with Github</a>
+
+                            </div>
+                        </div>
+                        <div class="form-group row mt-2">
+                            <div class="col-md-6 offset-md-4">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Enter your email">
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row mt-2">
+                            <div class="col-md-6 offset-md-4">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Enter ypur password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row mt-2">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0 mt-2">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
